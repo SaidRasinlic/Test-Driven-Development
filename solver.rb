84dcdfeb0ec4852ee051d82015ccfdcb9f556cd6
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-07-02 17:57:13 +0200

    Implement fizbuzz method by using TDD

--- solver.rb
+++ solver.rb
@@ -11,7 +11,12 @@ class Solver
   end
 
   def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 15).zero?
+
     return 'fizz' if (number % 3).zero?
+
     return 'buzz' if (number % 5).zero?
+
+    number.to_s
   end
 end
